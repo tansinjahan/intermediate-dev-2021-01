56d4b8d4e455f80ae6b7cce2d6c0068eb3b0bebf
Tansin Jahan <tansinjahan@Tansins-MacBook-Pro.local>
2021-01-16 18:30:08 -0500

    axios call in server side added to get json data

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
-
+import axios from 'axios';
+import * as fs from 'fs';
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
@@ -8,5 +9,30 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  //res.json([]);
+  axios.get('https://api.github.com/users/silverorange/repos')
+    .then(response => {
+      fs.readFile('./data/repos.json', 'utf8', (err, data) => {
+        if (err) {
+          res.status(400);
+          res.json({
+            status: 400,
+            message: 'A wild error appeared.',
+          })
+        } else {
+          const existingRepos = JSON.parse(data);
+          let merged = [...existingRepos, ...response.data].filter(repo => !repo.fork)
+          console.log("Merged: " + merged.length)
+          res.status(200);
+          res.json(merged)
+        }
+      })
+    })
+    .catch(error => {
+      res.status(400);
+      res.json({
+        status: 400,
+        message: 'A wild error appeared.',
+      })
+    })
 });
