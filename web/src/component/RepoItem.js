c7d39675d376fd87d7a5995e49372c6b7491c36e
Tansin Jahan <tansinjahan@Tansins-MacBook-Pro.local>
2021-01-16 19:17:52 -0500

    axios call succesfull in React and showrepo updated

--- web/src/component/RepoItem.js
+++ web/src/component/RepoItem.js
@@ -4,8 +4,11 @@ class RepoItem extends React.Component {
   render() {
     return (
       <div>
-        <p>Name</p>
-        <p>Description</p>
+        <p>Name: {this.props.dataRepoItem.name}</p>
+        <p>Description: {this.props.dataRepoItem.description}</p>
+        <p>language: {this.props.dataRepoItem.language}</p>
+        <p>forks: {this.props.dataRepoItem.forks}</p>
+        <p>created: {this.props.dataRepoItem.created_at}</p>
       </div>
     );
   }
--- web/src/component/ShowRepo.js
+++ web/src/component/ShowRepo.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import axios from 'axios';
 import { RepoItem } from './RepoItem';
 import { LanguageItem } from './LanguageItem';
 
 class ShowRepo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.repositories = [];
+    this.state = {
+      repo_data: [],
+      languages: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:4000/repos')
+      .then((response) => {
+        const repoList = response.data;
+        repoList.sort((a, b) =>
+          new Date(a.created_at) < new Date(b.created_at) ? 1 : -1
+        );
+        this.repositories = repoList;
+        this.setState({
+          repo_data: this.repositories,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div>
-        <RepoItem />
+        <h1>SilverOrange Exercise</h1>
         <LanguageItem />
+        {this.state.repo_data.map((repo) => (
+          <RepoItem key={repo.id} dataRepoItem={repo} />
+        ))}
       </div>
     );
   }
